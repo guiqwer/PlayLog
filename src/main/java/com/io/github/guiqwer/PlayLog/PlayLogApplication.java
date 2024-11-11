a5284380b2e006b941ba591b849616a2d64ce4c5
guiqwer <guilherme.souza0996@gmail.com>
2024-11-11 17:21:14 -0300

    Project Created

---
+++ src/main/java/com/io/github/guiqwer/PlayLog/PlayLogApplication.java
@@ -0,0 +1,13 @@
+package com.io.github.guiqwer.PlayLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/io/github/guiqwer/PlayLog/PlayLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.github.guiqwer.PlayLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
